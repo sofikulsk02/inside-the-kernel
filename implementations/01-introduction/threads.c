d31f3bddcd2f248341aab7f5066aacc02bc5e7f8
SOFIKUL SK <148935407+sofikulsk02@users.noreply.github.com>
2026-07-18 20:17:10 +0530

    refactoring

--- implementations/01-introduction/threads.c
+++ implementations/01-introduction/threads.c
@@ -7,14 +7,6 @@
 volatile int counter = 0;
 int loops;
 
-
-char test2(){
-    return 'a';
-}
-
-int fun(){
-    return 0;
-}
 void *worker(void *arg) {
     int i;
     for (i = 0; i < loops; i++) {
@@ -22,47 +14,7 @@ void *worker(void *arg) {
     }
     return NULL;
 }
-void testflw(){
-    cout<<"test flow check";
-}
-void message(int n){
-       return n==0 ? 0 : 1; 
-}
-void *worker(void *arg) {
-    int i;
-    for (i = 0; i < loops; i++) {
-        counter++;
-    }
-    return NULL;
-}
-int channel(){
-    return 1;
-}
 
-int chekker(int flag){
-    if(!flag){
-        return 1;
-    }
-    else return 0;
-}
-
-int changeCheck(int n){
-    return  n/10;
-}
-void check(char argc){
-    if(argc=='c'){
-        printf("check");
-    }
-    else printf("null")
-}
-void fail(int num){
-    if(num==0) {
-        printf("Failed A0221");
-    }
-    else{
-        printf("S0221");
-    }
-}
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "usage: threads <value>\n");
