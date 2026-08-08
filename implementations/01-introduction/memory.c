8e2b57c1d83673e95804eace8e9b83b6f10893df
SOFIKUL SK <148935407+sofikulsk02@users.noreply.github.com>
2026-08-08 23:33:44 +0530

    memory code updation

--- implementations/01-introduction/memory.c
+++ implementations/01-introduction/memory.c
@@ -3,13 +3,18 @@
 #include <stdlib.h>
 #include "common.h"
 
-int  main(int argc,char *argv[])
-{
+int  main(int argc,char *argv[]){
 int *p= malloc(sizeof(int)); //a1
  assert(p!= NULL);
+ printf("(%d) successfully allocated int on the heap\n", getpid());
+ printf("(%d) sizeof(int) = %lu byte(s)\n", getpid(), (unsigned long)sizeof(int));
+ printf("(%d) argc = %d\n", getpid(), argc);
+ if (argc > 1) {
+     printf("(%d) argv[1] = \"%s\"\n", getpid(), argv[1]);
+ }
  printf("(%d)memoryaddress of p:%08x\n",
 getpid(), (unsigned)p); //a2
-*p = 0; //a3
+*p = 0; //a3 
 while(1) {
  Spin(1);
  *p= *p + 1;
